daa083a3791eb0ea5621815618b06352591b6d7d
liukaixin <15713857919@163.com>
2025-06-21 16:04:43 +0800

    test: debug

---
+++ config/config.default.js
@@ -0,0 +1,23 @@
+// config/config.default.js
+exports.keys = '<此处改为你自己的 Cookie 安全字符串>';
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+// add middleware robot
+exports.middleware = [
+  'robot'
+];
+// robot's configurations
+exports.robot = {
+  ua: [/curl/i, /Baiduspider/i]
+};
